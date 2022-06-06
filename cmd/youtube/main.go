7f64af0bf2744553a545d1239f986268b58ad67e
Ronoaldo JLP <ronoaldo@gmail.com>
2022-06-06 17:53:20 -0300

    Add 'lives' and 'video-update' commands to youtube
    'lives' command lists all user broadcasts.
    'video-update' command updates a single video.

--- cmd/youtube/main.go
+++ cmd/youtube/main.go
@@ -1,37 +1,46 @@
 // Command youtube allows you to interact with the Youtube service using a
 // command line interface.
 //
-// Check the updated help with `youtube --help`:
+// Check the updated help with "youtube --help":
 //
-//     Usage of youtube:
-//       -channel channel_id
-//             The channel_id id to use.
-//       -cmd command
-//             The command to execute. Use -cmd="list" to show all commands. (default "list")
-//       -playlist playlist_id
-//             The playlist_id to use.
+//	Usage of youtube:
+//	-category category_id
+//		The category_id of the video to update.
+//	-channel channel_id
+//		The channel_id id to use.
+//	-cmd command
+//		The command to execute. Use -cmd="list" to show all commands. (default "list")
+//	-desc description
+//		The description of the video to update.
+//	-playlist playlist_id
+//		The playlist_id to use.
+//	-tags tags
+//		The list of tags separated by ',' to be used in the updated video.
+//	-title title
+//		The title of the video to update.
+//	-video video_id
+//		The video_id to use for editing.
 //
-//     Valid commands for -cmd are:
+//	Use one of the following values for the -cmd parameter:
+//		channels        list channels
+//		subscribers     list subscribers
+//		playlists       list playlists
+//		playlist-items  list videos in playlist
+//		playlist-dedup  remove duplicate videos from playlist
+//		video-update    update details about a video
+//		lives           list upcomming broadcasts
+//		list,help       show this message
+//		logout          revoke credentials
 //
-//     For channels:
-//             channels                list channels
-//             subscribers             list subscribers
-//
-//     For playlists:
-//             playlists               list playlists
-//             playlist-items          list videos in playlist
-//             dedup-playlist          remove duplicate videos from playlist
-//
-//     Misc:
-//             list                    show this message
-//             logout                  revoke credentials
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/ronoaldo/ogle"
@@ -39,11 +48,20 @@ import (
 	"google.golang.org/api/youtube/v3"
 )
 
-// Command line options
+// General command line options
 var (
 	command  string
 	playlist string
 	channel  string
+	video    string
+)
+
+// Video update command line options
+var (
+	videoTitle       string
+	videoDescription string
+	videoCategory    string
+	videoTags        string
 )
 
 // Globals
@@ -53,12 +71,14 @@ var (
 )
 
 func init() {
-	flag.StringVar(&command, "cmd", "list",
-		"The `command` to execute. Use -cmd=\"list\" to show all commands.")
-	flag.StringVar(&playlist, "playlist", "",
-		"The `playlist_id` to use.")
-	flag.StringVar(&channel, "channel", "",
-		"The `channel_id` id to use.")
+	flag.StringVar(&command, "cmd", "list", "The `command` to execute. Use -cmd=\"list\" to show all commands.")
+	flag.StringVar(&playlist, "playlist", "", "The `playlist_id` to use.")
+	flag.StringVar(&channel, "channel", "", "The `channel_id` id to use.")
+	flag.StringVar(&video, "video", "", "The `video_id` to use for editing.")
+	flag.StringVar(&videoTitle, "title", "", "The `title` of the video to update.")
+	flag.StringVar(&videoDescription, "desc", "", "The `description` of the video to update.")
+	flag.StringVar(&videoCategory, "category", "", "The `category_id` of the video to update.")
+	flag.StringVar(&videoTags, "tags", "", "The list of `tags` separated by ',' to be used in the updated video.")
 }
 
 func main() {
@@ -83,32 +103,33 @@ func main() {
 		listPlaylists(yt)
 	case "playlist-videos", "playlist-items":
 		listPlaylistVideos(yt)
-	case "dedup-playlist":
+	case "playlist-dedup", "dedup-playlist":
 		removeDuplicatesFromPlaylist(yt)
+	case "video-update":
+		videoUpdate(yt)
+	case "lives":
+		listLives(yt)
 	case "reauth", "logout":
 		logout()
-	case "list":
+	case "list", "help":
 		listCommands()
 	default:
-		flag.Usage()
+		log.Printf("Unknown command: '%s'", command)
+		listCommands()
 	}
 }
 
 var cmdList = `
-Valid commands for -cmd are:
-
-For channels:
-	channels		list channels
-	subscribers		list subscribers
-
-For playlists:
-	playlists		list playlists
-	playlist-items		list videos in playlist
-	dedup-playlist		remove duplicate videos from playlist
-
-Misc:
-	list			show this message
-	logout			revoke credentials
+Use one of the following values for the -cmd parameter:
+	channels	list channels
+	subscribers	list subscribers
+	playlists	list playlists
+	playlist-items	list videos in playlist
+	playlist-dedup	remove duplicate videos from playlist
+	video-update	update details about a video
+	lives		list upcomming broadcasts
+	list,help	show this message
+	logout		revoke credentials
 `
 
 func listCommands() {
@@ -118,15 +139,16 @@ func listCommands() {
 
 func listChannels(yt *youtube.Service) {
 	count := 0
-	w.Println("#", "ID", "NAME", "LANGUAGE", "URL", "SUBSCRIBERS", "VIDEOS", "VIEWS")
+	w.Println("#", "ID", "NAME", "LANGUAGE", "URL", "SUBSCRIBERS", "VIDEOS", "UPLOADS_PLAYLIST", "VIEWS")
 	defer w.Flush()
-	req := yt.Channels.List([]string{"id,snippet,statistics"}).Mine(true)
+	req := yt.Channels.List([]string{"id,snippet,statistics,contentDetails"}).Mine(true)
 	err := req.Pages(ctx, func(resp *youtube.ChannelListResponse) error {
 		for i := range resp.Items {
 			ch := resp.Items[i]
 			count++
 			w.Println(count, ch.Id, ch.Snippet.Title, ch.Snippet.DefaultLanguage, ch.Snippet.CustomUrl,
-				ch.Statistics.SubscriberCount, ch.Statistics.VideoCount, ch.Statistics.ViewCount)
+				ch.Statistics.SubscriberCount, ch.Statistics.VideoCount,
+				ch.ContentDetails.RelatedPlaylists.Uploads, ch.Statistics.ViewCount)
 		}
 		return nil
 	})
@@ -185,7 +207,7 @@ func listPlaylistVideos(yt *youtube.Service) {
 	}
 	count := 0
 
-	w.Println("#", "VIDEO", "STATUS", "URL")
+	w.Println("#", "PUBLISHED_AT", "VIDEO", "STATUS", "URL")
 	defer w.Flush()
 
 	req := yt.PlaylistItems.List([]string{"id,snippet,status,contentDetails"}).PlaylistId(playlist)
@@ -194,7 +216,7 @@ func listPlaylistVideos(yt *youtube.Service) {
 		for _, item := range resp.Items {
 			count++
 			url := "https://youtu.be/" + item.ContentDetails.VideoId
-			w.Println(count, item.Snippet.Title, item.Status.PrivacyStatus, url)
+			w.Println(count, item.Snippet.PublishedAt, item.Snippet.Title, item.Status.PrivacyStatus, url)
 		}
 		return nil
 	})
@@ -252,6 +274,88 @@ func removeDuplicatesFromPlaylist(yt *youtube.Service) {
 	log.Println("Playlist has no duplicate videos", len(uniqueVids), len(videos))
 }
 
+type byPubDate []*youtube.LiveBroadcast
+
+func (b byPubDate) Len() int           { return len(b) }
+func (b byPubDate) Less(i, j int) bool { return b[i].Snippet.PublishedAt < b[j].Snippet.PublishedAt }
+func (b byPubDate) Swap(i, j int)      { b[j], b[i] = b[i], b[j] }
+
+func listLives(yt *youtube.Service) {
+	count := 0
+
+	lives := make([]*youtube.LiveBroadcast, 0)
+	req := yt.LiveBroadcasts.List([]string{"id,snippet,contentDetails,status"}).BroadcastStatus("all")
+	err := req.Pages(ctx, func(resp *youtube.LiveBroadcastListResponse) error {
+		for _, item := range resp.Items {
+			lives = append(lives, item)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	sort.Sort(byPubDate(lives))
+
+	w.Println("#", "PUBLISHED_AT", "ID", "TITLE", "STATUS", "URL")
+	defer w.Flush()
+	for _, item := range lives {
+		count++
+		url := "https://studio.youtube.com/video/" + item.Id + "/livestreaming"
+		w.Println(count, item.Snippet.PublishedAt, item.Id, item.Snippet.Title,
+			item.Status.LifeCycleStatus, url)
+	}
+}
+
+func videoUpdate(yt *youtube.Service) {
+	if video == "" {
+		log.Fatal("No video_id provided. Use the -video flag to define what video we need to update.")
+	}
+
+	var videoPayload *youtube.Video
+	parts := []string{"id,snippet"}
+	req := yt.Videos.List(parts).Id(video)
+	err := req.Pages(ctx, func(resp *youtube.VideoListResponse) error {
+		if len(resp.Items) == 0 {
+			return fmt.Errorf("No vídeos matched the provided id '%s'", video)
+		}
+		videoPayload = resp.Items[0]
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	msg := "Updated Fields: "
+	if videoTitle != "" {
+		videoPayload.Snippet.Title = videoTitle
+		msg = msg + fmt.Sprintf("[title: %v]", videoTitle)
+	}
+	if videoDescription != "" {
+		videoPayload.Snippet.Description = videoDescription
+		msg = msg + fmt.Sprintf("[description: %v]", videoDescription)
+	}
+	if videoCategory != "" {
+		videoPayload.Snippet.CategoryId = videoCategory
+		msg = msg + fmt.Sprintf("[categoryId: %v]", videoCategory)
+	}
+	if videoTags != "" {
+		cleanedTags := []string{}
+		for _, tag := range strings.Split(videoTags, ",") {
+			cleanedTags = append(cleanedTags, strings.TrimSpace(tag))
+		}
+		videoPayload.Snippet.Tags = cleanedTags
+		msg = msg + fmt.Sprintf("[tags: %v]", strings.Join(cleanedTags, ","))
+	}
+
+	log.Printf("Updating video (id=%s). %s", video, msg)
+	_, err = yt.Videos.Update(parts, videoPayload).Do()
+	if err != nil {
+		log.Fatalf("Error updating video: %v", err)
+	}
+	log.Println("Vídeo updated")
+	// dumpjson(updatedVideo)
+}
+
 func logout() {
 	if err := ogle.RemoveTokenFromCache("youtube"); err != nil {
 		log.Fatalf("Unable to remove authentication token: %v", err)
@@ -281,4 +385,10 @@ func substr(s string, from, length int) string {
 		return s
 	}
 	return out + "..."
+}
+
+func dumpjson(v interface{}) {
+	enc := json.NewEncoder(os.Stderr)
+	enc.SetIndent("", "  ")
+	enc.Encode(v)
 }
